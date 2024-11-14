d50a91afa460c8cb0d632adc9e39c422bccacc51
alnals06 <lbisk2024@gmail.com>
2024-11-14 21:32:09 +0300

    Integrated helper functions as submodule

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function helloWorld(){
-    console.log("welcome to advanced github project");
-}
-helloWorld();
+// function helloWorld(){
+//     console.log("welcome to advanced github project");
+// }
+// helloWorld();
+import {greeting} from './external/helper-functions/greeting.js';
+
+console.log(greeting("github user"));
